f6d76f251e15004286fd1b1bde0385dab9d5e23e
maxamills <morrm046@gmail.com>
2024-06-22 16:37:23 +0100

    refactor: Remove generic endpoint for '/'

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,6 @@ dotenv.config();
 
 const port = process.env.PORT || 3001;
 
-app.get('/', (res: Response, req: Request) => res.send('Hello world'));
-
 app.listen(port, () =>
   console.log(`[server]: Server is running at http://localhost:${port}`)
 );
